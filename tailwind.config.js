8566e2f1801bd02a2e70fe0dacb3d4e252c9fbab
adam <adam.firshah@gmail.com>
2024-07-01 22:52:18 +0800

    commit 2 modules, stuck in slick

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '128': '32rem', // Adjust the value as needed
+        '256': '64rem', // Example for even larger sizes
+      },
+    },
   },
-  plugins: [],
-}
-
-module.exports = {
-    plugins: [
-        require('flowbite/plugin')
-    ],
-    // You can add other Tailwind CSS configuration options here
+  variants: {},
+  plugins: [
+    require('flowbite/plugin'),
+    // You can add other Tailwind CSS plugins here
+  ],
 }
